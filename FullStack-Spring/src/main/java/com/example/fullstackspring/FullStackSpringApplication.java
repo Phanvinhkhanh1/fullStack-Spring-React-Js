2c3da8b78c30e901479354cbc98e62c8dea741f8
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-03-03 23:33:57 +0700

    Create folder structure for backend

---
+++ FullStack-Spring/src/main/java/com/example/fullstackspring/FullStackSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackSpringApplication.class, args);
+    }
+
+}
